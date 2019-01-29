ceea99b619d20c052c39dd759a1953d3995477c5
Oliver <oliver.grant@gmail.com>
2019-01-29 17:01:57 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,24 +1,6 @@
 require 'pry'
 
 def reformat_languages(languages)
-  a_hash = { }
-  languages.each do |styles,keys_1|
-    keys_1.each do |langs,keys_2|
-      keys_2.each do |types,keys_3|
-        a_hash[langs] = { type: keys_3 }
-        if a_hash[langs][:style].key? == true
-          a_hash[langs][:style] << styles
-        else
-          a_hash[langs][:style] = styles
-        end
-        binding.pry
-      end
-    end
-  end
-  a_hash
-end
-
-def reformat_languages(languages)
   new_hash = {}
   languages.each do |style, lang|
     language.each do |name, type|
@@ -30,4 +12,4 @@ def reformat_languages(languages)
     end 
   end
   new_hash
-end	end
+end
