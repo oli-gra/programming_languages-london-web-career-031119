c512ef137d98c2b9ae8be6d8843f3421f2f1a491
Oliver <oliver.grant@gmail.com>
2019-01-29 16:46:55 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,14 +1,13 @@
 require 'pry'
 
 def reformat_languages(languages)
-  a_hash={}
+  a_hash = { }
   languages.each do |styles,keys_1|
     keys_1.each do |langs,keys_2|
       keys_2.each do |types,keys_3|
         a_hash[langs] = { type: keys_3 }
+        a_hash[langs][:style] ||= [ ]
         a_hash[langs][:style] << styles
-        
-        data['checks'] ||= [ ]
       end
     end
   end
