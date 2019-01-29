ff68060fafe3c677228dd4d467e6530e3295624c
Oliver <oliver.grant@gmail.com>
2019-01-29 16:34:09 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -5,7 +5,11 @@ def reformat_languages(languages)
   languages.each do |styles,keys_1|
     keys_1.each do |langs,keys_2|
       keys_2.each do |types,keys_3|
+        if a_hash.key? == true
+          a_hash[langs][:style] << styles
+        else
           a_hash[langs] = { type: keys_3, style: [styles] }
+        end
       end
     end
   end
